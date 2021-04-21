5b61380973b7476c5916d7e6fb7f0a2bbfee02f6
piwe2004 <piwe2004@gmail.com>
2021-04-21 10:22:20 +0900

    에러 리턴

---
+++ src/User.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function User({ user }) {
+    if (!user) {
+        return <div>로딩중</div>;
+    }
+
+    return (
+        <div>
+            <div>
+                <b>ID</b>: {user.id}
+            </div>
+            <div>
+                <b>Username:</b> {user.username}
+            </div>
+        </div>
+    );
+}
+
+export default User;
